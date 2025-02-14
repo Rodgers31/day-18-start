384e28b30616db51b8c7f3b5a151e0a35a960942
Rodgers31 <otienor986@gmail.com>
2025-02-13 23:59:07 -0600

    Added teacher solution

--- draw.py
+++ draw.py
@@ -7,21 +7,22 @@ t = Turtle()
 shapes = {"triangle": 3, "square": 4, "pentagon": 5, "hexagon": 6,
          "heptagon": 7, "octago": 8, "nanogon": 9, "decagon": 10}
 
+colors = ["lawn green", "slate blue", "maroon", "indigo"]
 
 def movers(amount, turn):
     for _ in range(amount):
-        rand_color = random.choices(range(255), k=3)
+        # rand_color = random.choices(range(255), k=3)
         # print('test', rand_color[0])
         # t.pencolor(int(rand_color[0]), int(rand_color[1]), int(rand_color[2]))
-        t.color(int(rand_color[0]), int(rand_color[1]), int(rand_color[2]))
         t.forward(100)
         t.right(turn)
 
 
 for shape in shapes:
-    rotate = math.floor(360 / shapes[shape])
+    rotate = 360 / shapes[shape]
     print(shapes[shape])
     print(rotate)
+    t.color(random.choices(colors))
     movers(int(shapes[shape]), rotate)
 
 
